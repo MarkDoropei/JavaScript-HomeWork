e430db370e95450efdb4cb7a44df8b9fac0016b7
Your Name <you@example.com>
2025-01-19 20:49:23 +0300

    first correction

---
+++ cycles.js
@@ -0,0 +1,36 @@
+// var name = "Александр";
+// if (name === "Марк"){ 
+//     console.log("Привет, " + name)
+// } else if (name === "Наталия"){
+//     console.log("Привет, Мама")
+// } else if (name === "Александр"){
+//     console.log("Привет, Папа")
+// } else {
+//     console.log("Привет, Незнакомец!")
+// }
+
+//for(var x = 3; x < 10000; x = x * 3) {
+//    console.log(x)
+//}
+
+//var x = 3
+//
+//while (x < 10000){
+//    console.log(x = x * 3);
+//}
+//console.log("конец")
+
+// var num = prompt("Введите число")
+
+// if (num % 2 == 1){
+//     alert("число нечетное")
+// } else {
+//     alert("число четное")
+// }
+
+// var name = prompt("Введите свое имя")
+
+// if ( name.length <= 4 ) {
+//     alert("твое имя короткое")
+// }   else {alert("твое имя длинное")}
+
---
+++ tasks.js
@@ -0,0 +1,57 @@
+/*var animals = ["Кот", "Рыба", "Лемур", "Комодский Варан"]
+for (var i = 0; i < animals.length; i++){
+    console.log(animals[i] + ' - прекрасное животное');
+}*/
+
+/*
+var alphabet = "абвгдеёжзиклмнопрстуфхцчшщъыьэюя";
+var randomString = '';
+var stringLength = 6; 
+var x = Math.floor(Math.random() * alphabet.length);
+for (var i = 0; i < stringLength ; i++){
+    randomString += alphabet[x]
+    var x = Math.floor(Math.random() * alphabet.length);
+}
+console.log(randomString)
+//
+var alphabet = "абвгдеёжзиклмнопрстуфхцчшщъыьэюя";
+var randomString = '';
+var stringLength = stringLength;
+while(randomString.length < 6 ){
+    randomString += alphabet[x]
+    var x = Math.floor(Math.random() * alphabet.length);
+}
+console.log(randomString)
+*/
+
+/*var input = 'javaScript is awesome';
+var output = '';
+for(var i = 0; i = input.length; i++ ){
+    if (input[i] === "a"){
+        output += '4';
+    } else if (input[i] === "e"){
+        output += '3';
+    } else if (input[i] === "i"){
+        output += '1';
+    } else if (input[i] === "0"){
+        output += '0';
+    } else {output += output[i]}
+    }
+console.log(output);*/
+
+/*var input = 'javaScript is awesome';
+var output = '';
+for(var i = 0; i < input.length ; i++ ){
+    var x = input[i];
+    if (x === "a"){
+        output += '4';
+    } else if (x === 'e'){
+        output += '3';
+    } else if (x === 'i'){
+        output += '1';
+    } else if (x === "0"){
+        output += '0';
+    } else {output += x}
+    }
+console.log(output);
+*/
