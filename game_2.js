1f6867bf07e129719dad7d67c850f3a3869bc523
Your Name <you@example.com>
2025-04-13 21:15:25 +0300

    New Gane options

--- game_2.js
+++ game_2.js
@@ -1,7 +1,7 @@
 var clicks = 0;
 
-var height = 400;
-var width = 400;
+var height = 800;
+var width = 800;
 
 
 var getRandomNumber = function (size) {
@@ -17,11 +17,15 @@ var getDistance = function(event, target) {
 var getDistanceHint = function (distance) {
     if (distance < 10){
         $('html').css('background', '#A60000')
-        return"Обожжешься!"}
+        return"Горишь!"}
+    else if(distance < 15){
+        $('html').css('background', '#800000')
+        return"Оббожешься"
+        }
     else if(distance < 20){
         $('html').css('background', '#FF4100')
         return"Очень горячо"
-    }
+        }
     else if(distance < 40){
         $('html').css('background', '#FFB100')
          return"Горячо"
@@ -29,6 +33,10 @@ var getDistanceHint = function (distance) {
     else if(distance < 80){
         $('html').css('background', '#FFD473')
         return"Тепло"
+    }
+    else if(distance < 120){
+        $('html').css('background', '#B0E0E6')
+        return"Прохладно"
     }
     else if(distance < 160){
         $('html').css('background', '#63AFD0')
@@ -38,9 +46,13 @@ var getDistanceHint = function (distance) {
         $('html').css('background', '#3BA3D0')
         return"Очень холодно"
     }
-    else{
+    else if(distance < 480){
         $('html').css('background', '#0772A1')
         return"Замерз"
+    }
+    else{
+        $('html').css('background', '#191970')
+        return"Сосулька"
     }
 }
 
@@ -48,6 +60,7 @@ var target = {
     x: getRandomNumber(width),
     y: getRandomNumber(height)
 }
+let attempts = prompt("За сколько попыток управитесь?")
 
 $("#map").click(function (event) {
     clicks++;
@@ -58,8 +71,17 @@ $("#map").click(function (event) {
 
     $("#distance").text(distanceHint);
 
+    $("#counter").text("У вас осталось вот столько " + (attempts - clicks) + " Попыток"); 
+
     if(distance < 8){
         alert("Вы нашли клад!")
+        var name = confirm("Желаете сыграть еще раз?");
+        if(name == true){
+            window.location.reload()
+        }
+    }
+    if((attempts - clicks) == 0){
+        alert("Игра окончена, вы проиграли !")
         var name = confirm("Желаете сыграть еще раз?");
         if(name == true){
             window.location.reload()
